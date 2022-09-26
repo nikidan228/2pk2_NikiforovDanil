021e8aa044d4c4b5f3297f2d2b5d851859baa486
nikiforov.ds2190 <nikiforov.ds2190@M409C-4R>
2022-09-26 10:41:40 +0300

    Добавьте файлы проекта.

---
+++ pz_5/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace pz_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int i;
+            //for (i = 10; i <= 60; i += 3)
+            //    Console.WriteLine(i);
+
+            //char b;
+            //for (b = 'Д'; b != 'Л'; b++)
+            //    Console.Write("{0}", b);
+
+            //int n;
+            //int m;
+            //for (n = 0; n < 5; n++)
+            //{
+            //    for (m = 0; m < 9; m++)
+            //        Console.Write('#');
+            //    Console.WriteLine('\n');
+            //}
+            //int a = 7;
+            //int b;
+            //for (b = 0; b != 100; b++)
+            //    if (b % a == 0)
+            //        Console.WriteLine(b);
+
+
+            //int i;
+            //int j = 82;
+            //for (i = 0; j - i != 35; i++) ;
+            //Console.WriteLine(i);
+
+
+        }
+    }
+}
