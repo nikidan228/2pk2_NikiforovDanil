79c0e77b42f541678d2a8756a00076adfb5a90ac
Данил Никифоров <nikidanya56@gmail.com>
2022-09-29 09:40:45 +0300

    ПЗ_6

---
+++ pz_6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //работа Никифорова Данила
+            ////Console.WriteLine("x\ty");
+            ////double x;
+            ////double y;
+            ////for (x = 1; x <= 3; x += 0.2)
+            ////{
+            ////    y = (8 * x) + (4 * Math.Pow(x, 4)) - (1.5 * Math.Pow(x, 3));
+            ////    Console.WriteLine(string.Format("{0} \t {1}", x, y));
+            ////    Console.WriteLine();
+            //}
+            //работа Мухамедзянова Руслана 
+            //double a=1.45;
+            //double b=2.3;
+            //double x;
+            //double y;
+            //for (x = 0; x <= 1; x += 0.1) ;
+            //{
+            //    y = x + b * x * Math.Sin(a);
+            //    Console.WriteLine(string.Format("{0} \t {1}", x, y));
+            //    Console.WriteLine();
+            //}
+                
+        }
+    }
+}
